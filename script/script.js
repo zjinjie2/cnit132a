0056d248666ed8d2cfc757c8cf2809b622f467e3
Jin Zhang <54287307+zjinjie2@users.noreply.github.com>
2024-01-31 14:55:10 -0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("#menu");
+const close = document.querySelector("#close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+  console.log("click");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
